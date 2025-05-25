a02969a6512dc0b9cfe330ca7a00b96c61ffb8a5
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-05-25 16:38:16 +0900

    test: add test of impls

--- enum-table/src/impls.rs
+++ enum-table/src/impls.rs
@@ -52,3 +52,87 @@ impl<K: Enumable, V, const N: usize> IndexMut<K> for EnumTable<K, V, N> {
         self.get_mut(&index)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::hash::{Hash, Hasher};
+
+    use super::*;
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    enum Color {
+        Red,
+        Green,
+        Blue,
+    }
+
+    impl Enumable for Color {
+        const VARIANTS: &'static [Self] = &[Color::Red, Color::Green, Color::Blue];
+    }
+
+    const TABLES: EnumTable<Color, &'static str, { Color::COUNT }> =
+        crate::et!(Color, &'static str, |color| match color {
+            Color::Red => "Red",
+            Color::Green => "Green",
+            Color::Blue => "Blue",
+        });
+
+    const ANOTHER_TABLES: EnumTable<Color, &'static str, { Color::COUNT }> =
+        crate::et!(Color, &'static str, |color| match color {
+            Color::Red => "Red",
+            Color::Green => "Green",
+            Color::Blue => "Blue",
+        });
+
+    #[test]
+    fn debug_impl() {
+        assert_eq!(
+            format!("{:?}", TABLES),
+            r#"{Red: "Red", Green: "Green", Blue: "Blue"}"#
+        );
+    }
+
+    #[test]
+    fn clone_impl() {
+        let cloned = TABLES.clone();
+        assert_eq!(cloned, TABLES);
+    }
+
+    #[test]
+    fn eq_impl() {
+        assert!(TABLES == ANOTHER_TABLES);
+        assert!(TABLES != EnumTable::new_with_fn(|_| "Unknown"));
+    }
+
+    #[test]
+    fn hash_impl() {
+        let mut hasher = std::collections::hash_map::DefaultHasher::new();
+        TABLES.hash(&mut hasher);
+        let hash1 = hasher.finish();
+
+        let mut hasher2 = std::collections::hash_map::DefaultHasher::new();
+        ANOTHER_TABLES.hash(&mut hasher2);
+        let hash2 = hasher2.finish();
+
+        assert_eq!(hash1, hash2);
+    }
+
+    #[test]
+    fn default_impl() {
+        let default_table: EnumTable<Color, &'static str, { Color::COUNT }> = EnumTable::default();
+        assert_eq!(default_table.get(&Color::Red), &"");
+        assert_eq!(default_table.get(&Color::Green), &"");
+        assert_eq!(default_table.get(&Color::Blue), &"");
+    }
+
+    #[test]
+    fn index_impl() {
+        assert_eq!(TABLES[Color::Red], "Red");
+        assert_eq!(TABLES[Color::Green], "Green");
+        assert_eq!(TABLES[Color::Blue], "Blue");
+
+        let mut mutable_table = TABLES.clone();
+        mutable_table[Color::Red] = "Changed Red";
+        assert_eq!(mutable_table[Color::Red], "Changed Red");
+    }
+}
